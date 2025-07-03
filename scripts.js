1380a7e6059449579d52ef65658a65a733847973
plum.io <124366797+Plumshubparall@users.noreply.github.com>
2025-07-03 10:44:03 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+Mostrar articulos //
+documento.getElementByid(´botón-mostrar-mostrar´).addEventlistener
+(´cli),funcion(){documento.getElementBid(´formulario´).estilo.monitor=´flexible¨});
+
+Ocultar articulo //
+Documento .getElementByid(´buttón-ocultar¨).addEventlisttener(´cli´,funcion(){
+  Documento.getElementBid(´formulario´).estilos.monitor=´ninguno´});
